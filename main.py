9a52149ea3813490deecc411e2a93ce4488061bb
kik-kik <kignasiak01@gmail.com>
2021-03-21 13:13:50 +0100

    fixed pylint errors + warnings and using external config file

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
-import os
-import pandas
+import logging
+
+import pandas  # type: ignore
+import yaml
 
 from schema_validator.models.log_model import LogModel
-from schema_validator.schema_validator import (
-    schema_validator,
-    schema_validator_validate_file,
-)
+from schema_validator.schema_validator import schema_validator_validate_file
 
 
-
-def main(
-    filename: str, show_model_schema: bool = False, error_log_file: str = None
-) -> None:
-    if show_model_schema:
+def main(filename: str, print_model_schema: bool = False) -> dict:
+    if print_model_schema:
         model_schema = LogModel.model_schema()
         print(model_schema)
 
-    if error_log_file:
-        if os.path.exists(error_log_file):
-            raise Exception(
-                "File already exists, please delete %s first to continue..."
-                % error_log_file
-            )
-
-    return schema_validator_validate_file(filename, error_log=error_log_file)
+    return schema_validator_validate_file(filename)
 
 
 if __name__ == "__main__":
-    # TODO: would be nice to have some sort of cli args parsing
-    FILENAME = "schema_validator/tests/test_data/test_data.json"
-    ERROR_LOG_FILE = "error_log.json"
-    report = main(FILENAME, show_model_schema=False, error_log_file=ERROR_LOG_FILE)
+    # TODO: would be nice to have some sort of cli args parsing,
+    # this could be  for overriding defaults from config.yaml
+    CONFIG_PATH = "schema_validator/config.yaml"
 
+    with open(CONFIG_PATH) as _config:
+        CONFIG = yaml.safe_load(_config)
 
-    # for r, v in report.values():
-    #     print(r)
-    #     print(f"{r}|{v['count of events']}")
-    #     break
+    FILENAME = CONFIG["file_to_validate"]
+    LOG_FILE = CONFIG["log_file"]
+    LOG_LEVEL = CONFIG["log_level"]
+    PRINT_MODEL_SCHEMA = CONFIG["print_model_schema"]
+    PRINT_REPORT = CONFIG["print_report"]
 
-    pandas_report = pandas.DataFrame.from_dict(report).fillna(0)
-    print(pandas_report)
+    # In new version of Python we could also use the new pattern matching feature here
+    LOG_LEVELS_MAPPING = {
+        "ERROR": logging.ERROR,
+        "INFO": logging.INFO,
+        "DEBUG": logging.DEBUG,
+    }
+
+    _log_level_options = LOG_LEVELS_MAPPING.keys()
+    if LOG_LEVEL not in _log_level_options:
+        raise ValueError(
+            f"Invalid option provided for log level, possible options: {list(_log_level_options)}"
+        )
+
+    _log_level = LOG_LEVELS_MAPPING[LOG_LEVEL]
+
+    logging.basicConfig(filename=LOG_FILE, level=logging.ERROR, filemode="w")
+
+    report = main(FILENAME, print_model_schema=PRINT_MODEL_SCHEMA)
+
+    if PRINT_REPORT:
+        pandas_report = pandas.DataFrame.from_dict(report)
+        print(pandas_report)
